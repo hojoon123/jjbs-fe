516b6d42434481641b13acd13a48c4c0852c6928
hojoon123 <rhzn5512@naver.com>
2024-10-18 14:32:18 +0900

    [Fix] 유저 조회 API 경로 수정

--- src/services/api/userApi.ts
+++ src/services/api/userApi.ts
@@ -1,16 +1,17 @@
 import { ProfileData, UserData } from '../types/userData';
 import { fetchWithToken } from '../utils/clientUtils';
 
+const BASE_URL = 'https://5138-222-102-164-25.ngrok-free.app';
 export const userApi = {
   // 사용자 프로필 정보 가져오기
   getUserProfile: async (): Promise<UserData> => {
-    const response = await fetchWithToken('http://localhost:8000/users/me/', 'GET');
+    const response = await fetchWithToken(`${BASE_URL}/users/me/`, 'GET');
     return response;
   },
 
   // 사용자 프로필 업데이트
   updateUserProfile: async (profileData: ProfileData): Promise<ProfileData> => {
-    const response = await fetchWithToken('http://localhost:8000/users/me/', 'PATCH', profileData);
+    const response = await fetchWithToken(`${BASE_URL}/users/me/`, 'PATCH', profileData);
     return response;
   },
 };
